1b1862d53bf6b1d621ac4148a4afeb63f08b094f
Faustino <faustino10.96@gmail.com>
2021-01-08 10:45:30 -0600

    Cambio conexion PDO

--- scripts/servidor/conexion_PDO.php
+++ scripts/servidor/conexion_PDO.php
@@ -1,11 +1,16 @@
+
+<!-- PHP CON PDO para la conexion y manejo de BD.-->
 <?php
     function conexionPDO($u, $p, $db) {
+        $u = "id15799552_faustino";
+        $p = "^w2N1Tp_TQH9}NW#";
+        $db = "id15799552_bdsimapaj";
         try {
             $dsn = "mysql:host=localhost;dbname=" . $db;
             $connection = new PDO($dsn, $u, $p);
             return $connection;
         } catch (PDOException $pdoe) {
-           error_log($pdeo->getMessage()); 
+           error_log($pdoe->getMessage()); 
         }
     }
 ?>
